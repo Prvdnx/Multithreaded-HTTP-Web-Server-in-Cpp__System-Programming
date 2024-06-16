d2267d8d53dea631b4de278c9f4fc946cdb4b938
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-06-17 02:46:47 +0700

    update server.cpp

--- server.cpp
+++ server.cpp
@@ -19,3 +19,16 @@ int	main(int argc, char const *argv[])
 
 	return (0);
 }
+
+/*
+ int server_fd = socket(domain, type, protocol);
+	"domain", also referred to as the address family - is the communication domain in which the socket should be created.
+Some of address families includes AF_INET (IP), AF_INET6 (IPv6), AF_UNIX (local channel, similar to pipes), AF_ISO
+(ISO protocols), and AF_NS (Xerox Network Systems protocols). AF_INET and AF_INET6 are most common.
+	"type", type of service. This is selected according to properties required by the application: for TCP transmission
+it can be SOCK_STREAM (for virtual circuit service), for UDP transmission it can be a SOCK_DGRAM (for datagram service),
+SOCK_RAW (for direct IP service). Check with your address family to see whether a particular service is available.
+	"protocol", indicates a specific protocol to use in supporting the sockets operation. This is useful in cases where
+some families may have more than one protocol to support a given type of service. The return value is a file descriptor
+(a small integer). The analogy of creating a socket is that of requesting a telephone line from the phone company.
+*/
